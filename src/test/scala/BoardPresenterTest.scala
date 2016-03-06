2d2e390d46530e9d5ad2166f1c8f5aad5c66df56
Mateusz Maciaszek <mateusz.maciaszekhpc@gmail.com>
2016-03-06 23:07:18 +0100

    test ConsolePresenter's representation of figures

--- src/test/scala/BoardPresenterTest.scala
+++ src/test/scala/BoardPresenterTest.scala
@@ -1,11 +1,12 @@
 import org.scalatest.{FlatSpec, Matchers}
+import BoardPresenter._
 
 class BoardPresenterTest extends FlatSpec with Matchers {
 
   "ConsolePresenter" should "be able to create plain text version of board" in {
     val board = Board()
 
-    val presentation = BoardPresenter.ConsolePresenter(board)
+    val presentation = ConsolePresenter(board)
 
     val expectedPresentation =
     """8 | Rb | Sb | Bb | Qb | Kb | Bb | Sb | Rb |
@@ -19,6 +20,15 @@ class BoardPresenterTest extends FlatSpec with Matchers {
   |  A |  B |  C |  D |  E |  F |  G |  H |"""
 
     presentation should be(expectedPresentation)
+  }
+
+  it should "create representation for every figure" in {
+    ConsolePresenter.figureNameSymbol(Figure.Pawn)   should be("P")
+    ConsolePresenter.figureNameSymbol(Figure.Rook)   should be("R")
+    ConsolePresenter.figureNameSymbol(Figure.Knight) should be("S")
+    ConsolePresenter.figureNameSymbol(Figure.Bishop) should be("B")
+    ConsolePresenter.figureNameSymbol(Figure.Queen)  should be("Q")
+    ConsolePresenter.figureNameSymbol(Figure.King)   should be("K")
   }
 
 }
